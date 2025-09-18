fe82ac82b37e0728a7fb56c82e512782ae619095
ShumyloYana <zolotushko1@gmail.com>
2025-09-18 10:01:42 +0300

    add js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { getContactsController, getContactsByIdController } from '../controllers/contacts.js';
-import { ctrlWrapper } from '../utils/ctrlWrapper';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
