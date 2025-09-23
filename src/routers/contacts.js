52c4ceae323acc727a9b18df0b9acd02e9105e9e
ShumyloYana <zolotushko1@gmail.com>
2025-09-23 21:52:02 +0300

    Add validation middleware and schemas created and updated

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,18 +1,20 @@
 import { Router } from 'express';
 import { getContactsController, getContactsByIdController, createContactController, updateContactController, deleteContactController } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-
+import { isValidId } from '../middlewares/isValidId.js';
+import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getContactsController));
 
-router.get('/:contactId', ctrlWrapper(getContactsByIdController));
+router.get('/:contactId', isValidId,ctrlWrapper(getContactsByIdController));
 
-router.post('/', ctrlWrapper(createContactController));
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
 
-router.patch('/:contactId', ctrlWrapper(updateContactController));
+router.patch('/:contactId', isValidId,validateBody(updateContactSchema), ctrlWrapper(updateContactController));
 
-router.delete('/:contactId', ctrlWrapper(deleteContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
